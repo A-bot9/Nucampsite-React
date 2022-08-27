de3d0d007f7ee297b13d9d5ae3ae34f35e7c7fdb
Adrian Leal <adleal956@gmail.com>
2022-08-27 12:36:11 -0700

    Partner components, and AboutPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 		<div className="App">
 			<Header />
 			<Routes>
+				<Route path="about" element={<AboutPage />} />
 				<Route path="/" element={<HomePage />} />
 				<Route path="contact" element={<ContactPage />} />
 				<Route path="directory" element={<CampsitesDirectoryPage />} />
