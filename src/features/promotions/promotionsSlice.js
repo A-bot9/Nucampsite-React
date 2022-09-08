019275ec73939acdf52d4acf39564e43f62bd692
Adrian Leal <adleal956@gmail.com>
2022-09-07 22:56:47 -0700

    Redux for All

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -1,6 +1,16 @@
 import { PROMOTIONS } from '../../app/shared/PROMOTIONS';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { promotionsArray: PROMOTIONS };
+
+const promotionsSlice = createSlice({
+	name: 'promotions',
+	initialState,
+});
+
+export const promotionsReducer = promotionsSlice.reducer;
 
 // Checks if featured is true otherwise its false
-export const selectFeaturedPromotion = () => {
-	return PROMOTIONS.find((promotion) => promotion.featured);
+export const selectFeaturedPromotion = (state) => {
+	return state.promotions.promotionsArray.find((promotion) => promotion.featured);
 };
