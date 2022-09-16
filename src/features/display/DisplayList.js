1dc805b41de95a42d90cce179be1ef80f9494742
Adrian Leal <adleal956@gmail.com>
2022-09-15 23:09:16 -0700

    Async Promotions, Async DisplayList

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,7 @@
 import { Col, Row } from 'reactstrap';
 // import DisplayCard from './DisplayCard';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
@@ -17,10 +19,17 @@ const DisplayList = () => {
 	return (
 		<Row>
 			{items.map((item, idx) => {
+				const { featuredItem, isLoading, errMsg } = item;
+				if (isLoading) {
+					return <Loading key={idx} />;
+				}
+				if (errMsg) {
+					return <Error errMsg={errMsg} key={idx} />;
+				}
 				return (
-					item && (
+					featuredItem && (
 						<Col md className="m-1" key={idx}>
-							<AnimatedDisplayCard item={item} />
+							<AnimatedDisplayCard item={featuredItem} />
 						</Col>
 					)
 				);
